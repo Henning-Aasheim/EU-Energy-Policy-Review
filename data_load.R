62dff9f77911f95445b5be697a14890fa914e551
Henning-Aasheim <henniner@gmail.com>
2025-07-22 12:30:53 +0200

    Included all datasets in this update

--- data_load.R
+++ data_load.R
@@ -1,7 +1,6 @@
 # Libraries --------------------------------------------------------------------
 
 library(countrycode)
-library(eurostat)
 library(rio)
 library(tidyverse)
 
@@ -26,10 +25,68 @@ country_codes <- list('BE', 'BG', 'CZ', 'DK', 'DE', 'EE', 'IE', 'EL', 'ES', 'FR'
 # import_list() takes the following arguments: file name, the sheets (by number),
 # and a command to bind the rows together.
 
-eu_nrg <- import_list('EU Energy Policy Review.xlsx', which = 6:33, rbind = T)
+eu_nrg <- import_list('data/EU Energy Policy Review.xlsx', which = 6:33, rbind = T)
 
 # Eurostat ---------------------------------------------------------------------
 
 # I load two main datasets: one showing capacity (euro_cap) and one showing
 # energy production (euro_prod). The actual downloading of the files are found
 # in energy_production_capacity.R
+
+## Loading SIEC-code data ------------------------------------------------------
+
+load('data/siec.Rdata')
+
+# Switzerland ------------------------------------------------------------------
+
+# Switzerland is not included in the Eurostat data, so this section loads Swiss 
+# data. 
+
+switzerland <- read.csv('data/switzerland.csv', sep = ';')
+
+## Loads capacity data ---------------------------------------------------------
+
+load('data/nrg.Rdata')
+
+# First processing:
+
+euro_cap <- nrg_retrive %>% 
+  filter(geo %in% country_codes & # Filters countries
+           plant_tec == 'CAP_NET_ELC' & # Filters capacity
+           siec %in% c('RA300', 'RA310', 'RA320', 'RA400', 'RA410', 'RA420', 'N9000')) %>% # Filters type of capacity
+  mutate(country = countrycode(geo, 'eurostat', 'country.name.en'), # Makes a country variable with country names
+         year = as.numeric(str_remove(TIME_PERIOD, '-01-01')), # Makes a year variable
+         gw_capacity = values/1000, # Converts MW to GW
+         source = 'EUROSTAT', # Makes a source variable (EUROSTAT)
+         scenario = 'obs', # Makes a scenario variable (observed values)
+         unit = 'GW') %>%  # Makes a unit variable
+  left_join(siec, by = 'siec') %>% # join with SIEC data to get a readable type variable 
+  rename(cap = gw_capacity) %>% # Renames gw_capacity
+  select(country, geo, siec, type, unit, source, scenario, year, cap) %>%  # Selects the needed variables
+  rbind(switzerland[which(switzerland$unit == 'GW'),]) # Adds data from Switzerland
+
+## Loads production data -------------------------------------------------------
+
+load('data/nrg_prod_nf.Rdata')
+
+
+
+euro_prod <- nrg_prod_nf_retrieve %>% # Filters countries
+  filter(geo %in% country_codes & # Filters countries 
+           nrg_bal == 'GEP' & # Filters bal-codes (All codes are found in nrg_bal.Rdata)
+           plants == 'TOTAL' & # Filters power plant type
+           operator == 'TOTAL', # Filters operator type
+         siec %in% c('RA300', 'RA310', 'RA320', 'RA400', 'RA410', 'RA420')) %>% # Filters type of capacity)
+  mutate(country = countrycode(geo, 'eurostat', 'country.name.en'), # Makes a country variable with country names
+         year = as.numeric(str_remove(TIME_PERIOD, '-01-01')), # Makes a year variable
+         source = 'EUROSTAT', # Makes a source variable (EUROSTAT)
+         scenario = 'obs', # Makes a scenario variable (observed values)
+         unit = 'GWh') %>%  # Makes a unit variable
+  left_join(siec, by = 'siec') %>% # join with SIEC data to get a readable type variable 
+  rename(cap = values) %>% # Renames values to cap
+  select(country, geo, siec, type, unit, source, scenario, year, cap) %>%  # Selects the needed variables
+  rbind(switzerland[which(switzerland$unit == 'GWh'),]) # Adds data from Switzerland
+
+## Remove temporary datasets ---------------------------------------------------
+
+rm(nrg_prod_nf_retrieve, nrg_retrive, siec, switzerland)
