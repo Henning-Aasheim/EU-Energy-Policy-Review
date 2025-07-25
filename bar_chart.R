ae05f517956f322586a7e59000bc2f91388af0d5
Henning-Aasheim <henniner@gmail.com>
2025-07-25 10:06:23 +0200

    Added new plot to bar_chart.R and deleted the main excel file from the data folder

--- bar_chart.R
+++ bar_chart.R
@@ -1,21 +1,18 @@
 # Libraries --------------------------------------------------------------------
 
 library(data.table)
+library(forcats)
 library(tidyverse)
-
-# Direct import of data (this must change) 
-
-eu_nrg <- import_list("EU Energy policy Review.xlsx", which = 6:33, rbind = T)
 
 # Creates a data.table structure from the data set which is imported 
 
-dta <- as.data.table(en_nrg)
+dta <- as.data.table(eu_nrg)
 
 #Values of interest which can be looked at for (here done as wind and solar)
 
 values_of_interest <- list("RA300", "RA400")
 
-#A for loop which iterates over the different countries where black is 2023 and grey is the target of 2030
+# A for loop which iterates over the different countries where black is 2023 and grey is the target of 2030
 
 for (x in values_of_interest){
   plot <- ggplot(dta[(year == 2023 | (year == 2030& target == 1)) & unit == "GW" & siec == x], #This subdivides the data
@@ -24,4 +21,13 @@ for (x in values_of_interest){
     geom_bar(stat = "identity", position = "identity", alpha = 0.5) +
     ggtitle("Capacity of GW by country in",x) +
     coord_flip()
-  print(plot)}
+  print(plot)
+}
+
+eu_nrg %>% 
+  filter(siec %in% c('RA310', 'RA320', 'RA420') & unit == 'GW' &
+         (year == 2023 | (year == 2030 & target == 1) | (year == 2040 & target == 1))) %>% 
+  ggplot(aes(x = fct_reorder(country, cap), y = cap, fill = factor(year))) +
+  geom_bar(stat = 'identity', position = 'dodge', alpha = .7) +
+  facet_wrap(~factor(siec), scale = 'free_x') +
+  coord_flip()
