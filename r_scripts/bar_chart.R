a1bf2ee70b963222f4b0afed73b8d37b1913e495
Henning-Aasheim <henniner@gmail.com>
2025-08-23 19:25:12 +0200

    Updated the bar chart with right numbers and made some design adjustments

--- r_scripts/bar_chart.R
+++ r_scripts/bar_chart.R
@@ -1,28 +1,7 @@
 # Libraries --------------------------------------------------------------------
 
-library(data.table)
 library(forcats)
 library(tidyverse)
-
-# Creates a data.table structure from the data set which is imported 
-
-dta <- as.data.table(eu_nrg)
-
-#Values of interest which can be looked at for (here done as wind and solar)
-
-values_of_interest <- list("RA300", "RA400")
-
-# A for loop which iterates over the different countries where black is 2023 and grey is the target of 2030
-
-for (x in values_of_interest){
-  plot <- ggplot(dta[(year == 2023 | (year == 2030& target == 1)) & unit == "GW" & siec == x], #This subdivides the data
-                 aes(x = country, y = cap, fill = as.factor(year))) +
-    scale_fill_manual(values = c("2023" = "Black", "2030" = "grey")) +
-    geom_bar(stat = "identity", position = "identity", alpha = 0.5) +
-    ggtitle("Capacity of GW by country in",x) +
-    coord_flip()
-  print(plot)
-}
 
 # Including Eurostat data ------------------------------------------------------
 
@@ -55,3 +34,4 @@ eu_nrg_giga %>%
        y = 'Capacity',
        fill = 'Year') +
   theme_minimal()
+
